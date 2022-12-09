98528a49381b45f9b92a3920f743bf7f99bc3fac
andremarinho <andre.bmarinho@hotmail.com>
2022-12-09 14:05:57 -0300

    Comenta linhas

--- cria_servidor_web.sh
+++ cria_servidor_web.sh
@@ -5,8 +5,8 @@ apt-get upgrade -y
 apt-get install apache2 -y
 apt-get install unzip -y
 echo "Segunda etapa: Montando a aplicação"
-cd /tmp
-wget https://github.com/denilsonbonatti/linux-site-dio/archive/refs/heads/main.zip
-unzip main.zip
-cd linux-site-dio-main
-cp -R * /var/www/html
+#cd /tmp
+#wget https://github.com/denilsonbonatti/linux-site-dio/archive/refs/heads/main.zip
+#unzip main.zip
+#cd linux-site-dio-main
+#cp -R * /var/www/html
