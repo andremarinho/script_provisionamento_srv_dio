47905a26143f8ad8fbfb8e80955e6a0b53b26391
andremarinho <andre.bmarinho@hotmail.com>
2022-12-09 14:03:41 -0300

    Retira linhas em branco

--- cria_servidor_web.sh
+++ cria_servidor_web.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
-
 echo "Primeira Etapa: Atualiza o servidor..."
-
 apt-get update
 apt-get upgrade -y
 apt-get install apache2 -y
 apt-get install unzip -y
-
 echo "Segunda etapa: Montando a aplicação"
-
 cd /tmp
 wget https://github.com/denilsonbonatti/linux-site-dio/archive/refs/heads/main.zip
 unzip main.zip
